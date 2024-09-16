63f4f8c0298cc3bac9d5046014049dcc817f1175
Bhavishya Sharma <bhavishya.sharma@bajajfinserv.in>
2024-09-17 03:15:27 +0530

    Initial Commit

---
+++ AnimatedTextField/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatedTextField
+//
+//  Created by Bhavishya Sharma on 17/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
